0ca8496566a137d2c652dfd527b1dca57d8e6254
Jose Remaggi <joseluisremaggi@gmail.com>
2023-04-14 20:56:49 -0300

    ejercicio jose remaggi semana 10 al 14

--- Tercer semestre/Abril/Laboratorio 3/semana-10-al-14.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-10-al-14.js
@@ -7,3 +7,14 @@ for(let i = 0; i < numeros.length; i++){
     console.log(numeros[i])
 }
 // FIN EJERCICIO ANABEL ALESCI
+
+// EJERCICIO JOSE REMAGGI
+//Ejercicio 2: Leer 5 numeros, gurdarlos en un arreglo y mostrarlos en el orden inverso al introducidos
+
+const numeros2 = [0,1,2,3,4];
+console.log(numeros2)
+
+for(let j = (numeros2.length)-1; j >= 0; j--){
+    console.log(numeros2[j])
+}
+//FIN EJERCICIO JOSE REMAGGI
