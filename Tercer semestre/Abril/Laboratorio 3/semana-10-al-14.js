071fb27bf769a422fdc1a88057a02467719b2325
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-04-15 10:08:40 -0300

    Update semana-10-al-14.js

--- Tercer semestre/Abril/Laboratorio 3/semana-10-al-14.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-10-al-14.js
@@ -18,3 +18,19 @@ for(let j = (numeros2.length)-1; j >= 0; j--){
     console.log(numeros2[j])
 }
 //FIN EJERCICIO JOSE REMAGGI
+
+//Ejercicio Yesica Lòpez
+
+//Leer 5 números, guardarlos en un arreglo y mostrarlos en el orden inverso al introducido.
+
+			let numeros=[20,5,15,12,27];
+			console.log("Números introducidos: ");
+			for(let i=0;i<numeros.length;i++){
+				console.log("|"+i+"| -> "+numeros[i]);
+			}
+            
+			console.log("Forma Inversa: ");
+			for(i=numeros.length-1;i>=0;i--){
+				console.log("|"+i+"| -> "+numeros[i]);
+			}
+//Fin Ejercicio Yesica Lòpez
