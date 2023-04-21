03299a9d658ecc8f18a973494b019e6cef174883
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-04-21 01:50:50 -0300

    Update semana-17-al-21.js
    Ejercicio Jesús Mercado, semana del 17 al 21

--- Tercer semestre/Abril/Laboratorio 3/semana-17-al-21.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-17-al-21.js
@@ -80,3 +80,18 @@ function cambiarValorObjeto(p1){
 cambiarValorObjeto(persona);
 console.log(persona);
 // Fin Ejercicio Anabel Alesci
+
+// Ejercicio Jesús Mercado
+
+// Sumar todos los argumentos
+let respuesta = sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; // arguments es para arreglos
+    }
+    return suma;
+}
+// Fin Ejercicio Jesús Mercado
