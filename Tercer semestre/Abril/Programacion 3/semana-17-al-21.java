b5c53688f248454632bfc7d89b375ddab497c8a8
alesciAnabel <anabelalesci24@gmail.com>
2023-04-22 16:45:32 -0300

    ejercicio anabel alesci java

--- Tercer semestre/Abril/Programacion 3/semana-17-al-21.java
+++ Tercer semestre/Abril/Programacion 3/semana-17-al-21.java
@@ -97,3 +97,47 @@ public class TestArgumentosVariables {
     }
 }
 // Fin Ejercicio Jose Remagi
+
+//Ejercicio Anabel Alesci
+package test;
+
+import enumeraciones.Dias;
+
+public class TestEnum {
+
+    public static void main(String[] args) {
+        System.out.println("Dia 1: " + Dias.LUNES);
+        indicarDia(Dias.LUNES);
+    }
+
+    private static void indicarDia(Dias dias) {
+        switch (dias) {
+            case LUNES:
+                System.out.println("Primer dia de la semana");
+                break;
+            case MARTES:
+                System.out.println("Segundo dia de la semana");
+                break;
+            case MIERCOLES:
+                System.out.println("Tercer dia de la semana");
+                break;
+            case JUEVES:
+                System.out.println("Cuarto dia de la semana");
+                break;
+            case VIERNES:
+                System.out.println("Quinto dia de la semana");
+                break;
+            case SABADO:
+                System.out.println("Sexto dia de la semana");
+                break;
+            case DOMINGO:
+                System.out.println("Septimo dia de la semana");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+
+    }
+}
+//Fin Ejercicio Anabel Alesci
