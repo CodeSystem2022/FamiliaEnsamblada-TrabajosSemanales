917907b079a64c0154facc232ec6fbd87e4113e1
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-04-22 20:01:45 -0300

    Update semana-17-al-21.js

--- Tercer semestre/Abril/Laboratorio 3/semana-17-al-21.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-17-al-21.js
@@ -258,3 +258,115 @@ cambiarValorObjeto(persona);
 console.log(persona);
 
 //Fin ejercicios Victoria Zaccaro
+
+//EJERCICIO MATÍAS VILLA
+
+//Hosting Se llama a la funcion antes de definirla o despues 
+//miFuncion(8,2);
+
+function miFuncion(a,b){
+    //console.log("Sumamos: "+(a+b));
+    return a+b; 
+
+}
+
+//llamamos a la función
+miFuncion(5,4);
+
+let resultado = miFuncion(6, 7);
+ 
+console.log(resultado);
+
+//Declaramos una función de tipo expresión o anonima
+let x = function(a,b){return a+b}; //necesita cierra con punto y coma
+resultado = x(5,6);//al llamarla se pone la variable y parentesis
+console.log(resultado);
+
+//funciones tipo self y invoking que se llaman a si mismas
+(function(a,b){
+    console.log("Ejecutando la funcion:"+ (a+b));
+
+
+})(9,6); //no se puede reutilizar
+
+//una fucnion es un tipo de dato
+
+console.log(typeof miFuncion);
+
+//tmb se pueden escribir como objetos 
+function miFuncionDos(a,b){
+    console.log(arguments.length); //nos dice cuentso argumentos tiene la funcion
+
+}
+
+miFuncionDos(5,7,3,5); 
+
+//too string
+var miFuncionTexto=miFuncionDos.toString();
+console.log(miFuncionTexto);
+
+//Funciones Flecha
+
+const sumarFuncionFlecha= (a,b)=> a+b; //como es constante no se puede cambiar el valor asignado
+resultado=sumarFuncionFlecha(3,7)
+console.log(resultado);
+//en funcion tipo flecha 
+//no usar la palabra reservada function, no se usan las llaves, ni la palabra reservada return
+
+//dentro de los parentesis van los parametros de la función 
+//cuando llamamos, usamos los argumentos (valores)
+
+//funcion tipo expresión
+let sumar = function(a=4,b=8){
+    console.log(arguments[0]); //muentras el primer parametro(A)
+    console.log(arguments[1]); //muestra el segundo parametro (B)
+   return a+b+ console.log(arguments[2])
+}
+resultado=sumar(3,5);
+console.log(resultado);
+
+//sumar todos los argumentso
+//concepto hosting, llamar a la funcion antes de definirla
+let respuesta = sumarTodo(5,4,5,5,6,5);
+console.log(respuesta);
+function sumarTodo(){
+    let suma=0 ;
+    for(let i = 0; i< arguments.length; i++){
+        suma+=arguments[i]; //argumets  es para arreglos
+    }
+    return suma;
+}
+
+//paso por valor y referencia
+
+//crear metodo de tipo primitivo
+
+let k = 10;
+function cambiarValor(a){ //paso por valor solo pasa una copia y no se modifica 
+    a=20;
+
+}
+
+cambiarValor(k);
+console.log(k);
+
+
+
+const persona={
+    nombre: 'Juan',
+    apellido: 'Lepe'
+}
+//paso por referencia
+console.log(persona);
+function cambiarValorObjeto(p1){
+    p1.nombre= 'Ignacio';
+    p1.apellido='Perez';
+
+}
+
+cambiarValorObjeto(persona)
+//al finalizar al función la variable p1 se destruye pero las modificaciones afectan al objeto persona
+
+console.log(persona);
+
+//FIN MATÍAS VILLA
