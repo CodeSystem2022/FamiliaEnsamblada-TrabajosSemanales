2df1003687eddef4b0a69ed0f56fbfd44baaed42
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-04-22 16:33:39 -0300

    Victoria Zaccaro

--- Tercer semestre/Abril/Programacion 3/semana-17-al-21.java
+++ Tercer semestre/Abril/Programacion 3/semana-17-al-21.java
@@ -26,3 +26,47 @@ public class TestArgumentosVariables {
 }
 
 //Fin ejercicio Gerardo Duckwitz
+
+//Ejercicios Victoria Zaccaro
+package test;
+
+import enumeraciones.Dias;
+
+public class TestEnumeraciones {
+
+    public static void main(String[] args) {
+        System.out.println("Dia 1: " + Dias.LUNES);
+        indicarDiaSemana(Dias.LUNES);
+    }
+
+    private static void indicarDiaSemana(Dias dias) {
+        switch (dias) {
+            case LUNES:
+                System.out.println("Primer dia de la semana");
+                break;
+            case MARTES:
+                System.out.println("Segundo dia de la semana");
+                break;
+            case MIERCOLES:
+                System.out.println("Tercer dia de la semana");
+                break;
+            case JUEVES:
+                System.out.println("Cuarto dia de la semana");
+                break;
+            case VIERNES:
+                System.out.println("Quinto dia de la semana");
+                break;
+            case SABADO:
+                System.out.println("Sexto dia de la semana");
+                break;
+            case DOMINGO:
+                System.out.println("Septimo dia de la semana");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+
+    }
+}
+//Fin ejercicios Victoria Zaccaro
