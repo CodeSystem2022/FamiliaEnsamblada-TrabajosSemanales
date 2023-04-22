801c9f661b348a93be3a601e35475cfba92e92b6
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-04-22 16:34:37 -0300

    Update semana-17-al-21.js

--- Tercer semestre/Abril/Laboratorio 3/semana-17-al-21.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-17-al-21.js
@@ -213,3 +213,48 @@ console.log(resultado);
 console.log(typeof miFuncion);
 
 //Fin ejercicios Gerardo Duckwitz
+
+//Ejercicios Victoria Zaccaro
+//Funcion de tipo expresion
+let sumar = function (a = 4, b = 8) {
+  console.log(arguments[0]); //Muestra el parametro a
+  console.log(arguments[1]); //Muestra el parametro b
+  console.log(arguments[2]);
+  return a + b + arguments[2];
+};
+resultado = sumar(3, 5, 9);
+console.log(resultado);
+//Sumar todos los argumentos
+let respuesta = sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+function sumarTodo() {
+  let suma = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    suma += arguments[i]; //arguments es para arreglos
+  }
+  return suma;
+}
+//Tipos primitivos
+let k = 10;
+function cambiarValor(a) {
+  //Paso por valor
+  a = 20;
+}
+
+cambiarValor(k);
+console.log(k);
+
+const persona = {
+  nombre: "Juan",
+  apellido: "Lepez",
+};
+console.log(persona);
+function cambiarValorObjeto(p1) {
+  p1.nombre = "Carlos";
+  p1.apellido = "Perez";
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
+
+//Fin ejercicios Victoria Zaccaro
