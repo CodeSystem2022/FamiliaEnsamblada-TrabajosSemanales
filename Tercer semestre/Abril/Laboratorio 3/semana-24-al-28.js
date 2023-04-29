030f9dd08288ea5e93bc83f11399f8a7d86a369d
Jose Remaggi <joseluisremaggi@gmail.com>
2023-04-29 14:24:37 -0300

    ejercicio semana 24 al 28 jose remaggi

--- Tercer semestre/Abril/Laboratorio 3/semana-24-al-28.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-24-al-28.js
@@ -24,3 +24,22 @@ console.log(persona.nombreCompleto());
 
 // Fin Ejercicio Anabel Alesci
 
+//Ejercicio Jose Remaggi
+
+let persona2 = new Object(); // Debe crear un nuevo objeto en memoria
+    persona2.nombre = 'Ash'
+    persona2.apellido = 'Ketchum'
+    persona2.edad = 10
+    persona2.telefono = '1234567890'
+    persona2.direccion = 'Pueblo Paleta 123'
+    console.log(persona2.telefono)
+    console.log(persona['apellido']) // accedemos como si fuera un arreglo
+    
+    //for_in y accedemos al objeto como si fuera un arreglo
+    for(propiedad in persona2){
+        console.log(propiedad);
+        console.log(persona2[propiedad])
+    }
+
+// FIn Ejercicio Jose Remaggi
+
