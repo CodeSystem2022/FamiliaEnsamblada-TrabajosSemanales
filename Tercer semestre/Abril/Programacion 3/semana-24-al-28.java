914b18f98c55c148676d0d0be2e1c785bcd4bc95
gfduck <66056030+gfduck@users.noreply.github.com>
2023-05-01 16:13:41 -0300

    Update semana-24-al-28.java

--- Tercer semestre/Abril/Programacion 3/semana-24-al-28.java
+++ Tercer semestre/Abril/Programacion 3/semana-24-al-28.java
@@ -118,3 +118,27 @@ public class TestForEach {
     }
 }
 //Fin Ejercicio Jose Remaggi
+
+//Ejercicio Gerardo Duckwitz
+
+package test;
+
+import domain.Persona;
+
+public class TestForEach {
+    public static void main(String[] args) {
+        int edades[] = {5,6,8,9}; //sintaxis resumida
+        for (int edad:edades) { //Sintaxis del ForEach
+            System.out.println("edad = "+ edad);
+        }
+        
+        Persona personas[] = {new Persona("Juan"), new Persona("Carla"), new Persona("Beatriz") };
+        
+        for (Persona persona:personas){
+            System.out.println("persona = " + persona);
+        }
+        
+    }
+}
+
+//Fin ejercicio Gerardo Duckwitz
