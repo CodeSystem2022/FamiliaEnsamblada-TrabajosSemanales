187e46ef13c056e92b400f04e2cf7c5d55cfca87
gfduck <66056030+gfduck@users.noreply.github.com>
2023-05-01 15:40:49 -0300

    Update semana-24-al-28.js

--- Tercer semestre/Abril/Laboratorio 3/semana-24-al-28.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-24-al-28.js
@@ -67,3 +67,46 @@ let personaString= JSON.stringify(persona);
 console.log(personaString);
 
 //Fin Ejercicio Yesica López
+
+
+//Ejercicio Gerardo Duckwitz
+let x = 10; //Variable de tipo primitiva
+console.log(x.length);
+console.log("Tipos primitivos");
+//Objeto
+let persona = {
+  nombre: "Carlos",
+  apellido: "Gil",
+  email: "cgil@gmail.com",
+  edad: 30,
+  nombreCompleto: function () {
+    return this.nombre + " " + this.apellido;
+  },
+};
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
+console.log(persona);
+console.log(persona.nombreCompleto());
+
+let persona2 = new Object(); //Debe crear un nuevo objeto en memoria
+persona2.nombre = "Juan";
+persona2.direccion = "Salada 14";
+persona2.telefono = "542618282821";
+console.log(persona2.telefono);
+console.log("Creamos un nuevo objeto");
+console.log(persona["apellido"]); //Accedemos como si fuera un arreglo
+console.log("Usamos el ciclo for in");
+//for in
+
+for (propiedad in persona) {
+  console.log(propiedad);
+  console.log(persona[propiedad]);
+}
+console.log("Cambiamos y corregimos el error");
+persona.apellida = "Betancud"; //Cambiamos dinamicamente el valor de un objeto
+delete persona.apellida; //Eliminamos el error
+console.log(persona);
+//Fin Gerardo Duckwitz
