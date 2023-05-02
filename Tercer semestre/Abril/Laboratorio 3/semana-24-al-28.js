4af5311c2bfb11eada3fd6185d58a6d9fc5a3979
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-05-01 22:20:47 -0300

    Update semana-24-al-28.js

--- Tercer semestre/Abril/Laboratorio 3/semana-24-al-28.js
+++ Tercer semestre/Abril/Laboratorio 3/semana-24-al-28.js
@@ -128,3 +128,44 @@ console.log(personaArray);
 let personaString = JSON.stringify(persona);
 console.log(personaString);
 //Fin Victoria Zaccaro
+
+
+//EJERCICIOS MATÍAS VILLA
+
+
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 30,
+    nombreCompleto: function(){ 
+        return this.nombre + ' ' + this.apellido;
+    }
+        
+    
+}
+console.log(persona.nombre)
+console.log(persona.apellido)
+console.log(persona.edad)
+console.log(persona.email)
+console.log(persona)
+console.log(persona.nombreCompleto());
+
+
+let persona2 = new Object();
+persona2.nombre = 'Juan';
+persona2.direccion ='Salada 14';
+persona2.telefono='3546156135156'
+console.log(persona2.telefono);
+console.log(persona['apellido']);
+
+//for in accedemos al objeto como si fuera un arreglo
+
+for(propiedad in persona){
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+    
+}
+
+//FIN MATÍAS VILLA
+//FIN EJERCICIO MATÍAS VILLA
