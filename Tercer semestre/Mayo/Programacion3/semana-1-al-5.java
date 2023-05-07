cf5bb0209a0e99c62d801ba6bd3dd1589311f761
gfduck <66056030+gfduck@users.noreply.github.com>
2023-05-07 18:25:00 -0300

    Ejercicio Gerardo

--- Tercer semestre/Mayo/Programacion3/semana-1-al-5.java
+++ Tercer semestre/Mayo/Programacion3/semana-1-al-5.java
@@ -159,3 +159,37 @@ public class Empleado {
     
 }
 //Fin ejercicio Victoria Zaccaro
+
+//Ejercicio Gerardo Duckwitz
+package test;
+
+import domain.*;
+
+public class TestInstanceOf {
+
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 10000);
+        determinarTipo(empleado1);
+        empleado1 = new Gerente("José", 5000, "Sistemas");
+        determinarTipo(empleado1);
+
+    }
+
+    public static void determinarTipo(Empleado empleado) {
+        if (empleado instanceof Gerente) {
+            System.out.println("Es de tipo Gerente");
+            Gerente gerente = (Gerente)empleado;
+            gerente.getDepartamento();
+            System.out.println("gerente = " + gerente.getDepartamento());
+            //((Gerente) empleado).getDepartamento();
+        } else if (empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado");
+             //Gerente gerente = (Gerente)empleado;
+            //System.out.println("gerente = " + gerente.getDepartamento());
+        } else if (empleado instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
+    }
+
+}
+//Fin Ejercicio Gerardo Duckwitz
