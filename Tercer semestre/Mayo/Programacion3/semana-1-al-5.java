2281a822c65661eab0039ef6f22a6c3063efcdf0
Jose Remaggi <joseluisremaggi@gmail.com>
2023-05-07 20:14:39 -0300

    ejercicio remaggi 1 al 5 mayo

--- Tercer semestre/Mayo/Programacion3/semana-1-al-5.java
+++ Tercer semestre/Mayo/Programacion3/semana-1-al-5.java
@@ -242,3 +242,36 @@ public class TestSobreescritura {
 }
 
 //Fin Ejercicio Anabel Alesci
+//Ejercicio Jose Remaggi
+package test;
+
+import domain.*;
+
+public class TestInstanceOf {
+
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Jose", 2500);
+        determinarTipo(empleado1);
+        empleado1 = new Gerente("Luis", 4000, "Producto");
+        determinarTipo(empleado1);
+
+    }
+
+    public static void determinarTipo(Empleado empleado) {
+        if (empleado instanceof Gerente) {
+            System.out.println("Es de tipo Gerente");
+            Gerente gerente = (Gerente)empleado;
+            gerente.getDepartamento();
+            System.out.println("gerente = " + gerente.getDepartamento());
+            //((Gerente) empleado).getDepartamento();
+        } else if (empleado instanceof Empleado) {
+            System.out.println("Es de tipo Empleado");
+             //Gerente gerente = (Gerente)empleado;
+            //System.out.println("gerente = " + gerente.getDepartamento());
+        } else if (empleado instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
+    }
+
+}
+//Fin ejercicio Jose Remaggi
