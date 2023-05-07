12e5a019937f32108b02178c0031d71af455a133
alesciAnabel <anabelalesci24@gmail.com>
2023-05-07 18:58:29 -0300

    ejercicio anabel alesci 1 al 5 de mayo

--- Tercer semestre/Mayo/Programacion3/semana-1-al-5.java
+++ Tercer semestre/Mayo/Programacion3/semana-1-al-5.java
@@ -193,3 +193,52 @@ public class TestInstanceOf {
 
 }
 //Fin Ejercicio Gerardo Duckwitz
+
+//Ejercicio Anabel Alesci
+package domain;
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+    
+    public Empleado(String nombre, double sueldo){
+        this.nombre=nombre;
+        this.sueldo=sueldo;
+    }
+    //Método para la sobreescritura
+    public String obtenerDetalles(){
+        return "Nombre: "+this.nombre+", Sueldo: "+this.sueldo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getSueldo() {
+        return sueldo;
+    }
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+}
+
+package domain;
+public class Gerente extends Empleado{
+    private String departamento;
+    
+    public Gerente(String nombre, double sueldo, String departamento){
+        super(nombre, sueldo);
+        this.departamento=departamento;
+    }
+}
+
+package test;
+import domain.Gerente;
+public class TestSobreescritura {
+    public static void main(String[] args) {
+        Gerente gerente1=new Gerente("Anabel",3000,"IT");
+        System.out.println("gerente1= "+gerente1.obtenerDetalles());
+    }
+}
+
+//Fin Ejercicio Anabel Alesci
