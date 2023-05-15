d75a1213fd97717ca9a433b2d53782ae8b7db562
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-05-15 14:59:21 -0300

    Update semana-8-al-12.java

--- Tercer semestre/Mayo/Programacion3/semana-8-al-12.java
+++ Tercer semestre/Mayo/Programacion3/semana-8-al-12.java
@@ -185,3 +185,72 @@ public abstract class FiguraGeometrica {
     }
 }
 // Fin Ejercicio Jesús Mercado
+
+
+//EJERCICIO MATÍAS VILLA
+
+
+package domain;
+
+import java.util.Objects;
+
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+    
+    public Empleado(String nombre, double sueldo){
+        this.nombre=nombre;
+        this.sueldo=sueldo;
+        
+        
+    }
+    
+    //Meotdo para la sobreescritura
+    public String obtenerDetalles(){
+        return "Nombre: "+this.nombre+",Suelod:"+this.sueldo;
+        
+        }
+    
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 13 * hash + Objects.hashCode(this.nombre);
+        hash = 13 * hash + (int) (Double.doubleToLongBits(this.sueldo) ^ (Double.doubleToLongBits(this.sueldo) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empleado other = (Empleado) obj;
+        if (Double.doubleToLongBits(this.sueldo) != Double.doubleToLongBits(other.sueldo)) {
+            return false;
+        }
+        return Objects.equals(this.nombre, other.nombre);
+    }
+    
+    
+    
+    
+}
+//FIN EJERCICIO MATÍAS VILLA.
+
+
