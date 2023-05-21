5c71552a578557d74ec038d18d942d5b111de8f9
gfduck <66056030+gfduck@users.noreply.github.com>
2023-05-20 21:55:17 -0300

    Update semana-15-al-19.js

--- Tercer semestre/Mayo/Laboratorio3/semana-15-al-19.js
+++ Tercer semestre/Mayo/Laboratorio3/semana-15-al-19.js
@@ -1,3 +1,56 @@
+//Ejercicios Gerardo Duckwitz
+// let persona3 = new Persona("Pablo", "Ramirez");
+class Persona {
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
+}
+class Empleado extends Persona {
+  //Clase hija
+  constructor(nombre, apellido, departamento) {
+    super(nombre, apellido);
+    this._departamento = departamento;
+  }
+  get departamento() {
+    return this._departamento;
+  }
+  set departamento(departamento) {
+    this._departamento = departamento;
+  }
+}
+
+let persona1 = new Persona("Pablo", "Ramirez");
+console.log(persona1.nombre);
+persona1.nombre = "Juan Pablo";
+console.log(persona1.nombre);
+// console.log(persona1);
+
+let persona2 = new Persona("Leandro", "Gomez");
+console.log(persona2.nombre);
+persona2.nombre = "Ana Laura";
+console.log(persona2.nombre);
+// console.log(persona2);
+
+let empleado1 = new Empleado("Maria", "Gimenez", "Sistemas");
+console.log(empleado1);
+console.log(empleado1.nombre);
+
+//Fin Ejercicio Gerardo Duckwitz
+
+
 // Inicio Trabajo Semanal Jesús Mercado
 // let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer, Persona is not defined
 
@@ -94,3 +147,5 @@ persona2.nombre = "Maria Laura";
 console.log(persona2.nombre);
 // console.log(persona2);
 //Fin Victoria Zaccaro
+
+
