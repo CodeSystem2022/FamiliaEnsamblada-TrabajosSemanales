ea7d328126197ee148d9ca6c6d9bf39cacfa4f17
alesciAnabel <anabelalesci24@gmail.com>
2023-05-27 23:34:24 -0300

    Ejercicio Java semana 22 al 26

--- Tercer semestre/Mayo/Programacion3/semana-22-al-26.java
+++ Tercer semestre/Mayo/Programacion3/semana-22-al-26.java
@@ -37,3 +37,74 @@ public class ImplementacionMySql implements IAccesoDatos{
     }
 }
 //Fin Ejercicio Yesica López
+
+//Ejercicio Anabel Alesci
+package accesodatos;
+
+
+public interface IAccesoDatos {
+    int MAX_REGISTRO = 10;
+    
+    //Metodo insertar es abstracto y sin cuerpo
+    void insertar();
+    
+    void listar();
+    
+    void actualizar();
+    
+    void eliminar();
+    
+}
+
+package accesodatos;
+
+
+public class ImplementacionMySql implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde MySql"); 
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Actualizar desde MySql");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Eliminar desde MySql");
+    }
+    
+package accesodatos;
+
+
+public class ImplementacionOracle implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde Oracle");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde Oracle");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Actualizar desde Oracle");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Eliminar desde Oracle");
+    }
+    
+}
+//Fin Ejercicio Anabel Alesci
