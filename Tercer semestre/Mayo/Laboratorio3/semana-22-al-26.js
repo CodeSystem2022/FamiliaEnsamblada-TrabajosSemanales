984be23b6f7e5f89bfa57e26ddd1072e1f56a8dc
alesciAnabel <anabelalesci24@gmail.com>
2023-05-27 22:14:22 -0300

    Subi ejercicio semana 22 al 26 de mayo. Alesci Anabel

--- Tercer semestre/Mayo/Laboratorio3/semana-22-al-26.js
+++ Tercer semestre/Mayo/Laboratorio3/semana-22-al-26.js
@@ -61,3 +61,61 @@ console.log(empleado1);
 console.log(empleado1.nombreCompleto());
 
 //Fin Ejercicio Yesica López
+//Ejercicio Anabel Alesci
+// let persona5 = new Persona("Laura", "Carrizo");
+class Persona {
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
+  nombreCompleto(){
+    return this._nombre+ ' '+ this._apellido;
+  }
+}
+class Empleado extends Persona {
+  //Clase hija
+  constructor(nombre, apellido, departamento) {
+    super(nombre, apellido);
+    this._departamento = departamento;
+  }
+  get departamento() {
+    return this._departamento;
+  }
+  set departamento(departamento) {
+    this._departamento = departamento;
+  }
+  //Sobreescritura
+  nombreCompleto(){
+    return super.nombreCompleto()+ ', '+ this._departamento;
+  }
+}
+
+let persona1 = new Persona("Laura", "Carrizo");
+console.log(persona1.nombre);
+persona1.nombre = "Nazarena Ruel";
+console.log(persona1.nombre);
+// console.log(persona1);
+
+let persona2 = new Persona("Luisina", "Scipioni");
+console.log(persona2.nombre);
+persona2.nombre = "Noe Rio";
+console.log(persona2.nombre);
+// console.log(persona2);
+
+let empleado1 = new Empleado("Ana", "Laura", "Nutricion");
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
+
+//Fin Ejercicio Anabel Alesci
