d2d0a7f50c6722e7e94e96c538d96023494426f1
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-05-29 10:12:39 -0300

    Update semana-22-al-26.js
    Ejercicios de Laboratorio III - JS, Clase n° 7: Clases parte 2, correspondientes a la semana del 22 al 26 de Mayo,

--- Tercer semestre/Mayo/Laboratorio3/semana-22-al-26.js
+++ Tercer semestre/Mayo/Laboratorio3/semana-22-al-26.js
@@ -353,3 +353,93 @@ console.log(empleado1.toString())
 console.log(persona1.toString());
 
     //####Fin EJercicios Matías Villa#####
+
+// Ejercicios Jesús Mercado
+// let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer, Persona is not defined
+
+class Persona{ // Clase padre
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+    
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    // Tarea:
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    nombreCompleto(){ // Método que devuelve el nombre completo
+        return this._nombre + ' ' + this._apellido;
+    }
+
+    // Sebreescribiendo el método de la clase padre (Object)
+    toString(){ // Regresa un String
+        // Se aplica el polimorfismo que significa = múltiples formas en tiempo de ejecución
+        // El método que se ejecuta depende si es una referencia de tipo padre o hija 
+        return this.nombreCompleto();
+    }
+}
+
+class Empleado extends Persona{ // Clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento; 
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+
+    // Sobreescritura
+    nombreCompleto(){
+        return super.nombreCompleto() + ', ' + this._departamento;
+    } 
+}
+
+let persona1 = new Persona('Martín','Perez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+// console.log(persona1);
+
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);
+persona2.nombre = 'María Laura';
+console.log(persona2.nombre);
+// console.log(persona2);
+
+// Ejercicio Métodos get y set para el apellido:
+console.log(persona1.apellido);
+persona1.apellido = 'Gomez';
+console.log(persona1.apellido);
+
+// Ejercicio Métodos get y set para el apellido:
+console.log(persona2.apellido);
+persona2.apellido = 'Schwarzenegger';
+console.log(persona2.apellido);
+
+let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
+
+// Object.prototype.toString Esta es la manera de acceder a atributos y métodos de manera dinámica
+console.log(empleado1.toString());
+console.log(persona1.toString());
+
+// Fin Ejercicios Jesús Mercado
