096cb62785d2c7550a52378e0714f2bf9098d33f
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-05-30 00:49:42 -0300

    Update semana-22-al-26.js

--- Tercer semestre/Mayo/Laboratorio3/semana-22-al-26.js
+++ Tercer semestre/Mayo/Laboratorio3/semana-22-al-26.js
@@ -443,3 +443,61 @@ console.log(empleado1.toString());
 console.log(persona1.toString());
 
 // Fin Ejercicios Jesús Mercado
+
+    //Inicio Ejercicio victoria
+class Persona 
+{ constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+  set apellido(nombre) {
+    this._apellido = apellido;
+  }
+  nombreVompleto(){
+    return this._nombre + ` `+ this._apellido;
+  }
+}
+//Clase  Hija
+class Empleado extends Persona {
+  constructor(nombre, apellido, departamento) {
+    super(nombre, apellido);
+    this._departamento = departamento;
+  }
+  get departamento() {
+    return this._departamento;
+  }
+  set departamento(departamento) {
+    this._departamento = departamento;
+  }
+ //Sobreescritura
+    nombreCompleto() {
+    return super.nombreCompleto() + ", " + this._departamento;
+  }
+}
+
+let persona1 = new Persona("Martin", "Perez");
+console.log(persona1.nombre);
+persona1.nombre = "Juan Carlos";
+console.log(persona1.nombre);
+// console.log(persona1);
+
+let persona2 = new Persona("Carlos", "Lara");
+console.log(persona2.nombre);
+persona2.nombre = "Maria Laura";
+console.log(persona2.nombre);
+// console.log(persona2);
+
+let empleado1 = new Empleado("Karina", "Bustos", "Administacion");
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
+
+//Fin Victoria Zaccaro
