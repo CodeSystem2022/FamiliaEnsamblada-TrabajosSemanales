0d57e62ad7964ab0292ed4f614fcd706150345e9
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-05-30 01:35:38 -0300

    Update semana-22-al-26.java

--- Tercer semestre/Mayo/Programacion3/semana-22-al-26.java
+++ Tercer semestre/Mayo/Programacion3/semana-22-al-26.java
@@ -183,8 +183,70 @@ public class TestInterfaces {
         datos.listar();
     }
 }
+   //###Fin Ejercicios Matías####
 
     
+    //INICIO ejercicio victoria zaccaro
+   
+    package accesodatos;
+public interface IAccesoDatos {
+    int MAX_REGISTRO=10;
+    //metodo insertar es abstaracto sin cuerpo
+    void insertar();
+    void listar();
+    void actualizar();
+    void eliminar();
+}
 
+//Implementacion oracle
+package accesodatos;
 
-    //###Fin Ejercicios Matías####
+public class ImplementacionOracle implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde Oracle");
+    }
+
+    @Override
+    public void listar() {
+     System.out.println("Listar desde Oracle");
+    }
+
+    @Override
+    public void actualizar() {
+     System.out.println("Actualizar desde Oracle");
+    }
+
+    @Override
+    public void eliminar() {
+    System.out.println("Eliminar desde Oracle");
+    }
+}
+//Implementacion MySQL
+package accesodatos;
+
+public class ImplementacionMySql implements IAccesoDatos {
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySql");
+    }
+    @Override
+    public void listar() {
+         System.out.println("Listar desde MySql");
+    }
+
+    @Override
+    public void actualizar() {
+         System.out.println("Actualizar desde MySql");
+    }
+
+    @Override
+    public void eliminar() {
+    System.out.println("Eliminar desde MySql");
+    }
+    
+}
+    //fin Victoria Zaccaro
+    
