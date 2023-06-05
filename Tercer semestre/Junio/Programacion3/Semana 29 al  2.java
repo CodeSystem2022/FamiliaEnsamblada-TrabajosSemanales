0fab8fc1a65dd3fffdc51b8cf842bb1260f852df
alesciAnabel <anabelalesci24@gmail.com>
2023-06-04 21:50:16 -0300

    ejercicio semana 29 al 2 de junio anabel alesci

--- Tercer semestre/Junio/Programacion3/Semana 29 al  2.java
+++ Tercer semestre/Junio/Programacion3/Semana 29 al  2.java
@@ -94,3 +94,22 @@ public class Persona implements Serializable{
     }
 }
 //Fin Ejericicio Jose Remaggi
+
+//Ejercicio Anabel Alesci
+package test;
+
+import domain.Persona;
+
+
+public class TestJavaBeans {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Jose");
+        persona.setApellido("Remaggi");
+        System.out.println("persona = " + persona);
+        
+        System.out.println("Persona nombre: "+persona.getNombre());
+        System.out.println("Persona apellido: "+persona.getApellido());
+    }
+}
+//Fin Ejercicio Anabel Alesci
