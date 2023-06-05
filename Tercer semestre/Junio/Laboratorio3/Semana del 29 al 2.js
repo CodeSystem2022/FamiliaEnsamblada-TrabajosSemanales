055f0cc5162bd03440e075142c5f04a262bf2dfa
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-06-04 23:54:15 -0300

    Update Semana del 29 al 2.js

--- Tercer semestre/Junio/Laboratorio3/Semana del 29 al 2.js
+++ Tercer semestre/Junio/Laboratorio3/Semana del 29 al 2.js
@@ -582,3 +582,125 @@ console.log(persona4.toString());
 let persona5 = new Persona("Federico", "Lopez");
 console.log(persona5.toString());
 //Fin Ejercicio Anabel Alesci
+
+//Ejercicio Victoria Zaccaro
+class Persona    // Clase padre
+{
+    static contadorPersonas = 0;    // Atributo estático
+    email = 'Valor default email';  // Atributo no estático
+    
+    static get MAX_OBJ() { // Este Método simula una constante
+        return 6;
+    }
+
+    constructor(nombre, apellido) {
+        this._nombre = nombre;
+        this._apellido = apellido;
+        Persona.contadorObjetosPersona++;
+        console.log("Se incrementa el contador " + Persona.contadorObjetosPersona);
+    }
+    get nombre() {
+        return this._nombre;
+    }
+
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+
+    get apellido() {
+        return this._apellido;
+    }
+
+    set apellido(apellido) {
+        this._apellido = apellido;
+    }
+
+    nombreCompleto() {
+        return this._nombre + ' ' + this._apellido;
+    }
+
+    //Sobreescribiendo el método de la clase padre (Object)
+    toString() {
+        return this.nombreCompleto();
+    }
+
+    static saludar() {
+        console.log('Saludos desde este método static');
+    }
+
+    static saludar2(persona) {
+        console.log(persona.nombre + ' ' + persona.apellido);
+    }
+}//Fin clase Persona
+
+class Empleado extends Persona { // Clase hija
+    constructor(nombre, apellido, departamento) {
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+
+    get departamento() {
+        return this._departamento;
+    }
+
+    set departamento(departamento) {
+        this._departamento = departamento;
+    }
+
+    // Sobreescritura
+    nombreCompleto() {
+        return super.nombreCompleto() + ', ' + this._departamento;
+    }
+}
+
+let persona1 = new Persona('Martín', 'Perez');
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos';
+console.log(persona1.nombre);
+// console.log(persona1);
+
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2.nombre);
+persona2.nombre = 'María Laura';
+console.log(persona2.nombre);
+// console.log(persona2);
+let empleado1 = new Empleado("Karina", "Bustos", "Administacion");
+console.log(empleado1);
+console.log(empleado1.nombreCompleto());
+
+// Object.prototype.toString Esta es la manera de acceder a atributos y métodos de manera dinámica
+console.log(empleado1.toString());
+console.log(persona1.toString());
+
+// persona1.saludar(); No se utiliza desde el objeto
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+// console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+// console.log(Persona.email); No puede acceder desde la clase
+console.log(persona1.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
+
+let persona3 = new Persona("Cinthia", "Meza");
+console.log(persona3.toString());
+
+console.log(Persona.MAX_OBJ);
+//Persona.MAX_OBJ = 10 //No se puede modificar
+console.log(Persona.MAX_OBJ);
+
+let persona4 = new Persona("Leonardo", "Sanchez");
+console.log(persona4.toString());
+let persona5 = new Persona("Jazmin", "Torres");
+console.log(persona5.toString());
+let persona6 = new Persona("Javier", "Flores");
+console.log(persona6.toString());
+//Fin Ejercicio Victoria Zaccaro
