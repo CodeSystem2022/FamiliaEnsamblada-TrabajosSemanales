aa67a8792fa303cba2809216d0bdf562371e4dc4
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-06-07 00:33:54 -0300

    Update Semana del 5 al 9.js

--- Tercer semestre/Junio/Laboratorio3/Semana del 5 al 9.js
+++ Tercer semestre/Junio/Laboratorio3/Semana del 5 al 9.js
@@ -1,1 +1,125 @@
+// Ejercicios correspondientes a Laboratorio III JS, de la semana del 05 al 09 de Júnio de 2023
+// Clase 9: Herencia
 
+// Ejercicio Jesús Mercado:
+// Corresponde al archivo PruebaPersonas.js, donde cargamos las 3 clases creadas previamente y
+// realizamos las pruebas sugeridas por la catedra
+class Persona{
+
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get idPersona(){
+        return this._idPersona;
+    }
+
+    get nombre(){
+        this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get edad(){
+        this._edad;
+    }
+
+    set edad(edad){
+        this._edad = edad;
+    }
+
+    toString(){
+        return `${this._idPersona} ${this._nombre} ${this._apellido} ${this._edad}`;
+    }
+}
+
+class Empleado extends Persona{
+
+    static contadorEmpleados = 0;
+
+    constructor(nombre, apellido, edad, sueldo){
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++Empleado.contadorEmpleados;
+        this._sueldo = sueldo;
+    }
+
+    get idEmpleado(){
+        return this._idEmpleado;
+    }
+
+    get sueldo(){
+        this._sueldo;
+    }
+
+    set sueldo(sueldo){
+        this._sueldo = sueldo;
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idEmpleado} ${this._sueldo}`;
+    }
+}
+
+class Cliente extends Persona{
+
+    static contadorClientes = 0;
+
+    constructor(nombre, apellido, edad, fecharegistro){
+        super(nombre, apellido, edad);
+        this._idCliente = ++Cliente.contadorClientes;
+        this._fechaRegistro = fecharegistro;
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fecharegistro(){
+        return this._fechaRegistro;
+    }
+
+    set fecharegistro(fecharegistro){
+        this._fechaRegistro = fecharegistro;
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idCliente} ${this._fechaRegistro}`;
+    }
+}
+
+// Prueba clase Persona:
+let persona1 = new Persona('Juan', 'Perez', 32);
+console.log(persona1.toString());
+
+let persona2 = new Persona('Carla', 'Ortega', 22);
+console.log(persona2.toString());
+
+// Prueba clase Empleado:
+let empleado1 = new Empleado('Pedro', 'Román', 18, 5000);
+console.log(empleado1.toString());
+
+let empleado2 = new Empleado('Jonas', 'Torres', 30, 7000);
+console.log(empleado2.toString());
+
+// Prueba clase Cliente:
+let cliente1 = new Cliente('Miguel', 'Zala', 29, new Date());
+console.log(cliente1.toString());
+
+let cliente2 = new Cliente('Natalia', 'Ortega', 22, new Date());
+console.log(cliente2.toString());
+// Fin Ejercicio Jesús Mercado
