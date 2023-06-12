e503457fd10b62810297f876c18c9cf47b25288f
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-06-12 01:29:17 -0300

    Victoria Semana del 5 al 9.java

--- Tercer semestre/Junio/Programacion3/Semana del 5 al 9.java
+++ Tercer semestre/Junio/Programacion3/Semana del 5 al 9.java
@@ -290,3 +290,70 @@ public class CalculadoraUTN {
     }
 }//Fin de la clase
 //Fin ejercicio Gerardo Duckwitz
+                           
+ //Ejercicio victoria zaccaro
+                           
+   import java.util.Scanner;
+
+public class CalculadoraUTN{
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("**Aplicacion Calculadora**");
+        while (true) {// Ciclo infinito
+            System.out.println("**Aplicacion Calculadora**");
+            // Mostramos el menu
+            System.out.println("""
+                    1.Suma
+                    2.Resta
+                    3.Multiplicacion
+                    4.Division
+                    5.Salir
+                    """);
+            System.out.println("Operacion a realizar?");
+            try {
+                var operacion = Integer.parseInt(entrada.nextLine());
+                if (operacion >= 1 && operacion <= 4) {
+                    System.out.print("Ingrese el valor del primer operando");
+                    var operando1 = Integer.parseInt(entrada.nextLine());
+                    System.out.print("Ingrese el valor delsegundo operando");
+                    var operando2 = Integer.parseInt(entrada.nextLine());
+                int  resultado;
+                    switch (operacion) {
+                        case 1 -> {
+                            resultado = operando1 + operando2;
+                            System.out.println("Resultado de la suma : " + resultado);
+                        }
+                        case 2 -> {
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado de la resta: " + resultado);
+                        }
+                        case 3 -> {
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado de la mulitplicacion:" + resultado);
+                        }
+                        case 4 -> {
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado de la division = " + resultado);
+                        }
+                        default -> System.out.printf("opcion erronea " + operacion);
+                    }// Fin de switch
+                } // Fin del if
+                else if (operacion == 5) {
+                    System.out.println("Hasta pronto..");
+                    break;// Rompe el ciclo y sale
+                } else {
+                    System.out.printf("Opcion erronea " + operacion);
+                }
+                // Imprimimos un salto de linea antes de volver a mostrar el menu
+                System.out.println("");
+            } // Fin del try, comienzo del catch
+
+            catch (Exception e) {
+                System.out.println("Ocurrio un error " + e.getMessage());
+                System.out.println();
+            }
+        } // Fin while
+    }// Fin main
+}// Fin clase
+//Fin ejercicio victoria zaccaro
