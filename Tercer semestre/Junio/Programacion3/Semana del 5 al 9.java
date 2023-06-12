609a6e5020ee71104bc274a20115c3d17602cc49
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-06-11 23:37:55 -0300

    Yesica Semana del 5 al 9.java

--- Tercer semestre/Junio/Programacion3/Semana del 5 al 9.java
+++ Tercer semestre/Junio/Programacion3/Semana del 5 al 9.java
@@ -139,4 +139,74 @@ public class CalculadoraUTN {
 }//Fin clase
 
 //Fin Ejercicio Jose Remaggi
+ 
+//Ejercicio Yesica López
+import java.util.Scanner;
 
+public class CalculadoraUTN {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        while(true){ //Ciclo infinito
+            System.out.println("********Aplicación Calculadora********");
+         
+            mostrarMenu();
+            
+            try {
+                var operacion = Integer.parseInt(entrada.nextLine());
+                if (operacion >= 1 && operacion <= 4) {
+                    ejecutarOperacion(operacion, entrada);
+                }//Fin del if
+                else if (operacion == 5) {
+                    System.out.printf("Hasta pronto...");
+                    break;//Rompe el ciclo y sale
+                } else {
+                    System.out.printf("Opción erronea " + operacion);
+                }
+                //Imprimimos un salto de linea antes de repetir el menu
+                System.out.println();
+            }catch(Exception e){ //Fin del try, comienzo del catch
+                System.out.println("Ocurrió un error " + e.getMessage());
+            }//Fin catch
+        }//Fin while
+    }//Fin main
+
+    private static void mostrarMenu(){
+        System.out.println("""
+                1.Suma
+                2.Resta
+                3.Multiplicacion
+                4.Division
+                5.Salir
+                """);
+        System.out.println("Operación a realizar? ");
+    } // Fin método mostrarMenu
+
+    private static void ejecutarOperacion(int operacion, Scanner entrada){
+        System.out.print("Digite el valor para el operador1: ");
+        var operando1 = Double.parseDouble(entrada.nextLine());
+        System.out.print("Digite el valor para el operando2: ");
+        var operando2 = Double.parseDouble(entrada.nextLine());
+        double resultado;
+        switch (operacion){
+            case 1 -> { //Suma
+                resultado = operando1 + operando2;
+                System.out.println("Resultado de la suma : " + resultado);
+            }
+            case 2 -> { //Resta
+                resultado = operando1 - operando2;
+                System.out.println("Resultado de la resta: " + resultado);
+            }
+            case 3 -> { //Multiplicación
+                resultado = operando1 * operando2;
+                System.out.println("Resultado de la multiplicación: " + resultado);
+            }
+            case 4 -> { //División
+                resultado = operando1 / operando2;
+                System.out.println("Resultado de la división: " + resultado);
+            }
+            default -> System.out.printf("Opcion erronea " + operacion);
+        } //Fin del switch
+    }//Fin metodo ejecutarOperacion
+}//Fin clase
+
+ //Fin Ejercicio Yesica López
