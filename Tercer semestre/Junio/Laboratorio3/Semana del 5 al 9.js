f9a0b563e8753bb5435730aed80c223981f061a2
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-06-12 00:30:30 -0300

    Victoria Semana del 5 al 9.js

--- Tercer semestre/Junio/Laboratorio3/Semana del 5 al 9.js
+++ Tercer semestre/Junio/Laboratorio3/Semana del 5 al 9.js
@@ -445,3 +445,111 @@ class Cliente extends Persona {
 }
 //Fin ejercicio Gerardo Duckwitz
 
+
+//Ejercicio victoria zaccaro
+class Persona{
+    static contadorPersonas = 0;
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+
+    get idPersona(){
+        return this._idPersona;
+    }
+
+    get nombre(){
+        this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get edad(){
+        this._edad;
+    }
+
+    set edad(edad){
+        this._edad = edad;
+    }
+
+    toString(){
+        return `${this._idPersona} ${this._nombre} ${this._apellido} ${this._edad}`;
+    }
+}
+class Empleado extends Persona{
+    static contadorEmpleados = 0;
+    constructor(nombre, apellido, edad, sueldo){
+        super(nombre, apellido, edad);
+        this._idEmpleado = ++Empleado.contadorEmpleados;
+        this._sueldo = sueldo;
+    }
+
+    get idEmpleado(){
+        return this._idEmpleado;
+    }
+
+    get sueldo(){
+        this._sueldo;
+    }
+
+    set sueldo(sueldo){
+        this._sueldo = sueldo;
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idEmpleado} ${this._sueldo}`;
+    }
+}
+
+class Cliente extends Persona{
+    static contadorClientes = 0;
+    constructor(nombre, apellido, edad, fecharegistro){
+        super(nombre, apellido, edad);
+        this._idCliente = ++Cliente.contadorClientes;
+        this._fechaRegistro = fecharegistro;
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fecharegistro(){
+        return this._fechaRegistro;
+    }
+
+    set fecharegistro(fecharegistro){
+        this._fechaRegistro = fecharegistro;
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idCliente} ${this._fechaRegistro}`;
+    }
+}
+//Prueba clase Persona
+let p1 = new Persona('Marisa', 'Zarate', 21);
+console.log(persona1.toString());
+let p2 = new Persona('Alina', 'Zarate', 35);
+console.log(persona2.toString());
+//Prueba clase Empleado
+let e1 = new Empleado('Lina', 'Tabares', 26, 5000);
+console.log(empleado1.toString());
+let e2 = new Empleado('Alejandra', 'Quara', 33, 300);
+console.log(empleado2.toString());
+//Prueba clase Cliente
+let c1 = new Cliente('Ramona', 'Hernadez', 20, new Date());
+console.log(cliente1.toString());
+let c2 = new Cliente('Jose', 'Gonzalez', 19, new Date());
+console.log(cliente2.toString());
+// fin ejercico victoria
