dee1fa58735b2ea1479a5ac59bdc350c41084c81
Jose Remaggi <joseluisremaggi@gmail.com>
2023-06-17 13:46:48 -0300

    ejercicio jose remaggi semana 12 al 16

--- Tercer semestre/Junio/Programacion3/Semana del 12 al 16.java
+++ Tercer semestre/Junio/Programacion3/Semana del 12 al 16.java
@@ -36,3 +36,40 @@ public class Persona {
 }
 //Fin Ejercicio Yesica López
 
+//Ejercicio Jose Remaggi
+public class Persona {
+   private int id;
+   private String nombre;
+   private String tel;
+   private String email;
+   private static int numeroPersonas = 0;
+   
+   //Constructor vacio
+   public Persona(){
+       this.id = ++Persona.numeroPersonas;
+   }
+   
+   //Constructor con parámetros (sobrecarga de constructores)
+   public Persona(String nombre, String tel, String email){
+       this();
+       this.nombre = nombre;
+       this.tel = tel;
+       this.email = email;
+   }
+   
+   @Override
+   public String toString(){
+       return "Persona{" +
+               "id=" + id +
+               ", nombre='" + nombre + '\'' +
+               ", tel='" + tel + '\'' +
+               ", email='" + email + '\'' +
+               '}';
+   }
+   
+   public static void main(String[] args) {
+       Persona persona1 = new Persona("Jose Remaggi", "2616846097", "jremaggi@yopmail.com");
+       System.out.println(persona1);
+   }
+}
+//Fin Ejercicio Jose Remaggi
