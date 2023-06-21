3935cd9913666c2a29cf739c71ce56aaba5ae67f
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-06-20 21:42:10 -0300

    VictoriaZaccaro

--- Tercer semestre/Junio/Programacion3/Semana del 12 al 16.java
+++ Tercer semestre/Junio/Programacion3/Semana del 12 al 16.java
@@ -182,3 +182,121 @@ public class ListadoPersonasApp {
     }//Fin del metodo ejecutaroperacion
 }
 //Fin ejercicio Gerardo Duckwitz
+
+//***Ejercicio victoria zaccaro**
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<Persona>();
+        //Empezamos con el menu
+        var salir = false;
+        while (!salir) {
+            mostrarMenu();
+            try{
+                salir = ejecutarOperacion(entrada, personas);
+            } catch (Exception e){
+                System.out.println("Error en la Operacion: "+e.getMessage());
+            }
+            System.out.println();
+        }//Fin del ciclo while
+    }//Fin metodo main
+
+    private static void mostrarMenu(){
+        //menu de  opciones
+        System.out.print("""
+                ** Listado de Personas **
+                1.Agregar 
+                2.Listar
+                3.Salir
+                """);
+        System.out.println("Digite una de las opciones: ");
+    }//Fin metodo mostrar menu
+
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        //Revisamos la opcion digitada a traves de un swtich
+        switch(opcion){
+            case 1 -> {//Agregar una persona a la lista
+                System.out.print("Digite el nombre: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el telefono: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el correo: ");
+                var email = entrada.nextLine();
+                //Creamos el objeto Persona
+                var persona = new Persona(nombre, tel, email);
+                //Agregamos a la persona a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: "+personas.size()+" elementos");
+            }//Fin del caso 1
+            case 2 -> {//Listar a las personas
+                System.out.println("Listado de personas: ");
+                //Mejoras con lambda y el metodo de referencia.ejemplo "::"
+                //personas.forEach((persona) -> System.out.println(persona));
+                personas.forEach(System.out::println);
+            }//Fin caso 2
+            case 3 -> {//Salir del ciclo
+                System.out.println("Hasta Pronto...");
+                salir = true;
+            }//Fin del caso 3
+            default -> System.out.println("Opcion incorrecta: "+opcion);
+        }//Fin del switch
+        return salir;
+    }//Fin metodo ejecutarOperacion
+}//Fin clase ListadoPersonasApp
+public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+    public Persona() {
+        this.id = ++numeroPersonas;
+    }
+    public Persona(String nombre, String tel, String email) {
+        this();
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getTel() {
+        return this.tel;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String toString() {
+        int var9999 = this.id;
+        return "Persona{id=" + var9999 + ", nombre='" + this.nombre + "', tel='" + this.tel + "', email='" + this.email + "'}" + super.toString();
+    }
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("victoriaZaccaro", "33999222", "zacvictoria@gmail.com");
+        System.out.println(persona1);
+    }
+}
+// **Fin Ejercicio victoria zaccaro**
