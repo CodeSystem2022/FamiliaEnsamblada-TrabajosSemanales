41a892e4c52b076e455e6e37e417b7935fc99f28
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-06-25 19:41:17 -0300

    Yesica Semana del 19 al 23.java

--- Tercer semestre/Junio/Programacion3/Semana del 19 al 23.java
+++ Tercer semestre/Junio/Programacion3/Semana del 19 al 23.java
@@ -60,3 +60,44 @@ public static void main(String[]args){
 }//Fin main
 }//Fin de clase
 //Fin ejercicio Anabel Alesci
+
+
+//Ejercicio Yesica López
+package UTN.conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion{
+    public static Connection getConnection(){
+        Connection conexion = null;
+        //Variables para conectarnos a la base de datos
+        var baseDatos = "estudiantes2022";
+        var url = "jdbc:mysql://localhost:3306/"+baseDatos;
+        var usuario = "root";
+        var password = "admin";
+        
+        //cargamos la clase del driver mysql en memoria
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url,usuario,password);
+        }catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Ocurrió un error en la conexión: "e.getMessage());
+        }//fin try catch
+        return conexion;
+    }//fin método Connection
+}
+
+package UTN;
+import UTN.conexion.Conexion;
+public class Main{
+    public static void main(String[] args){
+        var conexion = Conexion.getConnection();
+        if (conexion != null)
+           System.out.println("Conexión exitosa: "+conexion);
+        else
+           System.out.println("Error al conectarse!");
+    }//Fin main
+}//Fin de clase
+
+//Fin Ejercicio Yesica López
