ae09292bf58bf5935aa6d47e75746e1eb625a69c
Jose Remaggi <joseluisremaggi@gmail.com>
2023-06-25 12:37:40 -0300

    ejercicio semana 19 al 23 Remaggi

--- Tercer semestre/Junio/Programacion3/Semana del 19 al 23.java
+++ Tercer semestre/Junio/Programacion3/Semana del 19 al 23.java
@@ -1,1 +1,18 @@
+//ejercicio Jose Remaggi
+import java.sql.Connection
+public class Conexion{
+    public static Connection getConnection(){
+        Connection conexion = null;
+        //Variables para conectarnos a la base de datos
+        var baseDeDatos = "estudiantes2022";
+        var url = "jdbc:mysql://localhost:3306/"+baseDeDatos;
+        var usuario = "root";
+        var password = "admin";
 
+        //cargamos la clase del driver mysql en la memoria
+        class.forName("com.mysql.cj.jdbc.Driver");
+        conexion = Driver.getConnection(url,usuario,password);
+    }
+
+}
+//fin ejercicio Jose Remaggi
