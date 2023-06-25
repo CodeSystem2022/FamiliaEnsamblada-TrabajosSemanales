7697c7a32ed734d7da44ddd77fccb66d7408fc14
alesciAnabel <anabelalesci24@gmail.com>
2023-06-25 13:05:09 -0300

    ejercicio 19 al 23 de junio alesci

--- Tercer semestre/Junio/Programacion3/Semana del 19 al 23.java
+++ Tercer semestre/Junio/Programacion3/Semana del 19 al 23.java
@@ -1,5 +1,9 @@
 //ejercicio Jose Remaggi
-import java.sql.Connection
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Conexion{
     public static Connection getConnection(){
         Connection conexion = null;
@@ -9,10 +13,50 @@ public class Conexion{
         var usuario = "root";
         var password = "admin";
 
-        //cargamos la clase del driver mysql en la memoria
-        class.forName("com.mysql.cj.jdbc.Driver");
-        conexion = Driver.getConnection(url,usuario,password);
+        try {
+            //cargamos la clase del driver mysql en la memoria
+            Class.forName("com.mysql.cj.jdbc.Driver");
+         
+        conexion = DriverManager.getConnection(url,usuario,password);
+        }catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
     }
+        return conexion;
 
 }
+}
 //fin ejercicio Jose Remaggi
+    
+//ejercicio Anabel Alesci
+
+public class Conexion{
+    public static Connection getConnection(){
+        Connection conexion = null;
+        //Variables para conectarnos a la base de datos
+        var baseDeDatos = "estudiantes2022";
+        var url = "jdbc:mysql://localhost:3306/"+baseDeDatos;
+        var usuario = "root";
+        var password = "admin";
+
+        try {
+            //cargamos la clase del driver mysql en la memoria
+            Class.forName("com.mysql.cj.jdbc.Driver");
+         
+        conexion = DriverManager.getConnection(url,usuario,password);
+        }catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+    }//fin try catch
+        return conexion;
+    }// fin getConnection
+}//fin clase Conexion
+
+public class main{
+public static void main(String[]args){
+    var conexion = Conexion.getConnection();
+    if (conexion != null){
+        System.out.println("Conexion exitosa"+ conexion);
+    }else
+        System.out.println("Error al conectarse");
+}//Fin main
+}//Fin de clase
+//Fin ejercicio Anabel Alesci
