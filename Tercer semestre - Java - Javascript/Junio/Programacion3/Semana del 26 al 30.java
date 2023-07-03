a8198a3904d1b7d66276311b5fcf821b9681f7c4
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-07-02 23:25:56 -0300

    Yesica Semana del 26 al 30.java

--- Tercer semestre - Java - Javascript/Junio/Programacion3/Semana del 26 al 30.java
+++ Tercer semestre - Java - Javascript/Junio/Programacion3/Semana del 26 al 30.java
@@ -491,3 +491,136 @@ public class Estudiante {
 }
 // Fin Ejercicio Jesús Mercado
 
+//Ejercicio Yesica López
+package UTN.datos;
+import UTN.dominio.Estudiante;
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class EstudianteDAO {
+    //Metodo Listar
+    public List <Estudiante> listarEstudiantes(){
+        List<Estudiante> estudiantes = new ArrayList<>();
+        PreparedStatement ps; 
+        ResultSet rs;
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2 ORDER BY estudiantes2023";  
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("idestudiantes2023"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                estudiantes.add(estudiante); 
+            }
+        } catch(Exception e){
+            System.out.println("Ocurrió un error al seleccionar datos"+e.getMessage());
+
+        }
+        finally {
+            try{
+                con.close();
+            }catch (Exception e){
+                System.out.println("Ocurrió un error al cerrar la conexión: "+e.getMessage());
+            }//Fin catch
+        }//Fin finally
+        return estudiantes;
+    }//Fin método listar
+    
+    //Método agregar un nuevo estudiante
+    public boolean agregarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "INSERT INTO estudiantes2022 (nombre,apellido,telefono,email)VALUES(?,?,?,?)";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1,estudiante.getNombre());
+            ps.setString(2,estudiante.getApellido());
+            ps.setString(3,estudiante.getTelefono());
+            ps.setString(4,estudiante.getEmail());
+            ps.execute();
+            return true; 
+        }catch(Exception e){
+            System.out.println("Ocurrió un error al agregar estudiante: "+e.getMessage());
+        }//Fin catch
+        finally{
+            try{
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar la conexión: "+e.getMessage());
+            }//Fin catch
+            }//Fin finally
+        return false;
+    }//Fin método agregarEstudiante
+        
+
+    //Método para modificar estudiante
+    public boolean modificarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "UPDATE estudiante2822 SET nombre=?, apellido=?, telefono=?, email=? WHERE idestudiantes2022=?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1,estudiante.getNombre());
+            ps.setString(2,estudiante.getApellido());
+            ps.setString(3,estudiante.getTelefono());
+            ps.setString(4,estudiante.getEmail());
+            ps.setString(5,estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
+        }catch(Exception e){
+            System.out.println("Error al modificar estudiante: "+e.getMessage());
+        }//Fin catch
+        finally{
+            try{
+                con.close();
+            } catch(Exception e){
+                System.out.println("Error al cerrar la conexión: "+e.getMessage());
+            }//Fin catch
+        }//Fin finally
+        return false;
+        
+}//Fin método modificarEstudiante
+    
+   public static void main(String[] args) {
+        var estudianteDao = new EstudianteDAO();
+        //Modificar estudiante
+        var estudianteModificado = new Estudiante(1,"Juan Carlos","Juarez","5789033809","juan@mail.com");
+        var modificado = estudianteDao.modificarEstudiante(estudianteModificado);
+        if(modificado)
+            System.out.println("Estudiante modificado: "+estudianteModificado);
+        else
+            System.out.println("No se modifico el estudiante: "+estudianteModificado);
+        
+        //Agregar estudiante
+        //var nuevoEstudiante= new Estudiante("Carlos","Lara","5433435452", "carlos@mail.com");
+        //var agregado= estudianteDao.agregarEstudiante(nuevoEstudiante);
+        //if(agregado)
+        //    System.out.println("Estudianteagregado: "+nuevoEstudiante);
+        //else
+        //    System.out.println("No se ha agregado estudiante: "+nuevoEstudiante);
+        
+        //Listar los estudiantes
+        System.out.println("Listado de estudiantes: ");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println);//Función lambda para imprimir
+        
+        //Buscar por id
+        //var estudiante1= new Estudiante(1);
+        //System.out.println("Estudiante antes de la busqueda: "+estudiante1);
+        //var encontrado = estudianteDao.buscarEstudiantePorId(estudiante1);
+        //if(encontrado)
+        //    System.out.println("Estudiante encontrado: "+estudiante1);
+        //else
+        //    System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante);
+        
+    }
+}
+//Fin Ejercicio Yesica López
